7939dab94f8ac8600056394845c25e4f3775ad27
dgkwon <dgkwon90@gmail.com>
2023-03-30 18:20:39 +0900

    feat: docker file 작성
    - docker container 생성을 위한 docker file 추가
    - docker build 시 build-arg로 client, server 분기 할 수 있도록 작성
    - 소스 구조 수정 보완
    - docker-compose 추가
    - docker env 추가
    
    feature #6

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -5,16 +5,36 @@ import (
 	"fmt"
 	"go-health/api/health"
 	"log"
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+var (
+	// GitCommit, BuildTime, Get infos at build time the golang.
+	GitCommit string
+	BuildTime string
+)
+
+// buildInfoPrint display git short commit id, build time.
+// It is used to know the version of the launched application.
+func buildInfoPrint() {
+	// default log
+	log.Printf("Build Information : %v at %v\n", GitCommit, BuildTime)
+	log.Println("Started at :", time.Now().Format(time.RFC3339))
+}
+
 func SendPing() {
+	serverIP := os.Getenv("SERVER_IP")
+	if len(serverIP) <= 0 {
+		serverIP = "9999" // default
+	}
+
 	i := 0
 	for {
-		conn, connErr := grpc.Dial(":9999", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		conn, connErr := grpc.Dial(serverIP, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if connErr != nil {
 			log.Fatalf("did not connect: %v", connErr)
 		}
@@ -38,5 +58,6 @@ func SendPing() {
 }
 
 func main() {
+	buildInfoPrint()
 	SendPing()
 }
